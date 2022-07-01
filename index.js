d4c7b71fdbf5392e28cccb143b02a164bbf0521b
Rodri Balma <roolo.balmaceda7@gmail.com>
2022-06-30 22:09:37 -0300

    agregamosJs+estilos

--- index.js
+++ index.js
@@ -1,136 +1,70 @@
-/*import productos from './store/productos';*/
-
-const productos = [
-  {
-    titulo: "NikeBlack",
-    img: "https://www.moov.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dwdab6dee0/products/NI_DC4068-001/NI_DC4068-001-1.JPG",
-    precio: 180.25,
-    categoria: "calzado",
-    tallesXStock: [
-      {
-        talle: "s",
-        stock: "2",
-      },
-    ],
-  },
-  {
-    titulo: "NikePaste",
-    img: "https://www.moov.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dw35accab8/products/NI_DC4068-600/NI_DC4068-600-1.JPG",
-    precio: 150.25,
-    categoria: "calzado",
-    tallesXStock: [
-      {
-        talle: "s",
-        stock: "2",
-      },
-    ],
-  },
-  {
-    titulo: "NikeBoy",
-    img: "https://www.moov.com.ar/on/demandware.static/-/Sites-dabra-catalog/default/dwa20df339/products/NI_DJ3624-400/NI_DJ3624-400-1.JPG",
-    precio: 350.25,
-    categoria: "calzado",
-    tallesXStock: [
-      {
-        talle: "s",
-        stock: "2",
-      },
-    ],
-  },
-  {
-    titulo: "NikePurple",
-    img: "https://www.moov.com.ar/dw/image/v2/BDTF_PRD/on/demandware.static/-/Sites-dabra-catalog/default/dw50afdaec/products/NI_DD1068-003/NI_DD1068-003-1.JPG?sw=400&sh=400",
-    precio: 450.5,
-    categoria: "calzado",
-    tallesXStock: [
-      {
-        talle: "s",
-        stock: "2",
-      },
-    ],
-  },
-  {
-    titulo: "NikeAirBlue",
-    img: "https://www.moov.com.ar/dw/image/v2/BDTF_PRD/on/demandware.static/-/Sites-dabra-catalog/default/dw12ce1188/products/NI_CJ0575-100/NI_CJ0575-100-1.JPG?sw=400&sh=400",
-    precio: 460.5,
-    categoria: "calzado",
-    tallesXStock: [
-      {
-        talle: "s",
-        stock: "2",
-      },
-    ],
-  },
-];
-
 function cargarProductos() {
-  let cardDeck = document.getElementById("productos");
-  productos.forEach((producto) => {
-    cardDeck.innerHTML += `<div class="card">
-        <img src="${producto.img}" class="card-img-top" alt="${producto.titulo}">
-        <div class="card-body">
-          <h5 class="card-title">${producto.titulo}</h5>
-          <p class="card-text">${producto.precio}</p>
-        </div>
-      </div>`;
-  });
+  const lista = document.querySelector("#listado");
+  fetch("./data.json")
+    .then((res) => res.json())
+    .then((data) => {
+      data.forEach((producto) => {
+        const li = document.createElement("div");
+        li.innerHTML = `
+        <div class="col mb-4">
+      <div class="card .h-100">
+  <img  src="assets/img/productos/${producto.imagen}" class="card-img-top" alt="${producto.nombre}">
+  <div class="card-body">
+    <h5 class="card-title">${producto.id} - ${producto.nombre} </h5>
+    <p class="card-text">$${producto.precio}</p>
+    <a href="#" class="btn btn-primary" onclick = "agregar('${producto.id}','${producto.nombre}');
+    return false;">Agregar</a>
+  </div>
+ </div>
+</div>
+      `;
+        lista.append(li);
+      });
+    });
 }
 
 window.onload = function () {
   cargarProductos();
+  mostrarCarrito();
 };
-  const lista = document.querySelector("#listado");
-fetch("./data.json")
-  .then((res) => res.json())
-  .then((data) => {
-    data.forEach((producto) => {
-      const li = document.createElement("div");
-      li.innerHTML = `
-      <div class="product" id="${producto.id}">
-      <div class="img">
-      <img class="imagen" src="assets/img/productos/${producto.imagen}">
-    </div>
-    <div class="title">${producto.nombre}</div>
-    <div class="price">$${producto.precio}</div>
-    <div class="code">Codigo: <span>${producto.id}</span></div>
-    <button onclick = "agregar('${producto.id}','${producto.nombre}')">Agregar</button>
-    </div>
-    `;
-      lista.append(li);
-    });
+
+function agregar(id, nombre) {
+  Swal.fire({
+    position: "center",
+    icon: "success",
+    title: nombre + " agregado correctamente",
+    showConfirmButton: false,
+    timer: 1500,
   });
 
-  function agregar(id,nombre){
-    addEventListener("click", () => {
-      Swal.fire ({
-        position: "center",
-        icon: "success",
-        title: nombre+" agregado correctamente",
-        showConfirmButton: false,
-        timer: 1500,
-      })
-      });
-      agregando= id+" "+nombre;
+  const agregando = `<li class="list-group-item"> ${id} - ${nombre}</li>`;
 
-tipo = typeof(carrito)
-if(tipo == "undefined"){
-  carrito = agregando;
-}else{
-  carrito = carrito + "<br>" +agregando;
-  localStorage.setItem("carrito",carrito)
+  const carrito = localStorage.getItem("carrito");
 
+  if (carrito == null) {
+    localStorage.setItem("carrito", agregando);
+  } else {
+    localStorage.setItem("carrito", carrito + agregando);
+  }
+
+  mostrarCarrito();
 }
 
-mostrarCarrito()
-  }
+function mostrarCarrito() {
+  const carrito = localStorage.getItem("carrito");
+  document.getElementById("carrito").innerHTML = carrito;
+}
 
-  function mostrarCarrito(){
-    carrito = localStorage.getItem("carrito")
-document.getElementById('carrito').innerHTML = carrito;
-  }
+function vaciarCarrito() {
+  localStorage.removeItem("carrito");
+  localStorage.clear("carrito");
+  document.getElementById("carrito").innerHTML = "";
 
-  function vaciarCarrito(){ 
-    localStorage.removeItem("carrito")
-    localStorage.clear("carrito")
-  document.getElementById('carrito').innerHTML = "";
-  }
+  Swal.fire({
+    icon: "warning",
+    position: "center",
+    title: "vaciado correctamente",
+    showConfirmButton: false,
+    timer: 1500,
+  });
+}
